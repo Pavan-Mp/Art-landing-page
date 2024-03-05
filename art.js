328b1001e5f33f47e035d407e5d605229f4a750d
Pavan-Mp <pavanyadavak777coorg@gmail.com>
2024-03-05 13:03:23 +0530

    first commit

---
+++ art.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+      let sidebar = document.querySelector('.sidebar')
+          sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+    let sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = 'none'
+}
